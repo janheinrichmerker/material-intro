55f1b1aa6437d6ddc92fffe2cf9ae69562bc6223
Heinrich Reimer <heinrich@heinrichreimersoftware.com>
2016-02-21 22:34:44 +0100

    Fixed error handling in SimpleSlide.Builder

--- library/src/main/java/com/heinrichreimersoftware/materialintro/slide/SimpleSlide.java
+++ library/src/main/java/com/heinrichreimersoftware/materialintro/slide/SimpleSlide.java
@@ -96,8 +96,8 @@ public class SimpleSlide extends Slide {
         }
 
         public SimpleSlide build(){
-            if(background == 0 || title == 0)
-                throw new IllegalArgumentException("You must set at least a title and background.");
+            if (background == 0 || title == 0 || layout == 0)
+                throw new IllegalArgumentException("You must set at least a title, background and layout.");
             return new SimpleSlide(this);
         }
     }
