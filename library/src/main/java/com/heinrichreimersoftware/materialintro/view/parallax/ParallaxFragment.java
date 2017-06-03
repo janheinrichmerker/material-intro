80a75535be4569874657c75a47795dd321b73247
Heinrich Reimer <heinrich@heinrichreimer.com>
2017-06-03 15:22:19 +0200

    Update ParallaxFragment.java

--- library/src/main/java/com/heinrichreimersoftware/materialintro/view/parallax/ParallaxFragment.java
+++ library/src/main/java/com/heinrichreimersoftware/materialintro/view/parallax/ParallaxFragment.java
@@ -19,7 +19,7 @@ public class ParallaxFragment extends Fragment implements Parallaxable {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        parallaxableChildren = findParallaxLayouts(view);
+        findParallaxLayouts(view);
     }
 
     public void findParallaxLayouts(View root) {
@@ -37,7 +37,6 @@ public class ParallaxFragment extends Fragment implements Parallaxable {
                 }
             }
         }
-        return parallaxableChildren;
     }
 
     @Override
