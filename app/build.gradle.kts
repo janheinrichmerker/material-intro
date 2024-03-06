1c7dcf2df5dd681f63b9fdd8c8f952950307f88c
Jan Heinrich Reimer <heinrich@reimer.family>
2024-03-06 02:39:02 +0000

    Modernize build

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    id("com.palantir.git-version")
+}
+
+android {
+    namespace = "com.heinrichreimersoftware.materialintro.demo"
+
+    compileSdk = 29
+
+    defaultConfig {
+        applicationId = "com.heinrichreimersoftware.materialintro.demo"
+        minSdk = 15
+        targetSdk = 29
+        versionCode = 20000
+        versionName = gitVersion()
+    }
+
+    lintOptions {
+        abortOnError = false
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    dataBinding {
+        enabled = true
+    }
+}
+
+dependencies {
+    implementation(project(":library"))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("com.google.firebase:firebase-core:16.0.6")
+    implementation("com.google.firebase:firebase-crash:16.2.1")
+}
---
+++ library/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library")
+    id("com.palantir.git-version")
+}
+
+android {
+    namespace = "com.heinrichreimersoftware.materialintro"
+
+    compileSdk = 29
+
+    defaultConfig {
+        minSdk = 15
+        targetSdk = 29
+        versionCode = 20000
+        versionName = gitVersion()
+    }
+
+    lintOptions {
+        abortOnError = false
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+}
